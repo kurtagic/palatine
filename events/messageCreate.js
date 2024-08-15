48005c4b1daaaba375374dfab5add28a50a0ace4
teo kurtagic <teo@sl-king.com>
2024-08-16 01:02:16 +0200

    fdafsf

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,22 +10,22 @@ module.exports = {
         const db = new sqlite3.Database("./db/main.db");
 
         const userID = message.author.id;
-        const messageContentLength = message.content.length;
 
         getUser(db, userID).then(({ experience, level }) => {
                 experience += getRandomInteger(5, 15);
 
-                const experienceLimit = getNewExperienceNeeded(level);
+                let experienceLimit = getNewExperienceNeeded(level);
 
-				if(experience >= experienceLimit)
-                {
+				if(experience >= experienceLimit) {
                     experience -= experienceLimit;
                     level++;
+					experienceLimit = getNewExperienceNeeded(level);
                     message.channel.send(`LEVEL UP!`);
                 }
 
                 db.run('UPDATE experience_ranks SET experience = ?, level = ? WHERE user_id = ?', [experience, level, userID]);
-				message.channel.send(`experience: **${experience}**\nlevel: **${level}**`);
+				message.channel.send(`experience: **${experience}**xp, level: **${level}**, needed: **${experienceLimit}**xp`);
+	
 		})
 		.finally(() => {
 			db.close();
@@ -55,7 +55,7 @@ function getRandomInteger(min, max) {
 function getNewExperienceNeeded(level) {
     const base_xp = 50;
     const newLevel = level + 1;
-    const factor = 2;
-
-    return base_xp * (newLevel ^ factor);
+    const factor = 1.5;
+	
+	return Math.round(base_xp * (newLevel ** factor));
 }
